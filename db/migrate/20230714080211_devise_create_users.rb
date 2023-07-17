947dadbdf37e99f76c65b824927783fae1eb2c4b
naoyaaaa <23nakajima.naoya@gmail.com>
2023-07-17 12:42:24 +0900

    Update 20230714080211_devise_create_users.rb

--- db/migrate/20230714080211_devise_create_users.rb
+++ db/migrate/20230714080211_devise_create_users.rb
@@ -1,6 +1,6 @@
 # # frozen_string_literal: true
 
-# class DeviseCreateUsers < ActiveRecord::Migration[7.0]
+class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 #   def change
 #     # create_table :users do |t|
 #       ## Database authenticatable
@@ -41,5 +41,5 @@
 #     add_index :users, :reset_password_token, unique: true
 #     # add_index :users, :confirmation_token,   unique: true
 #     # add_index :users, :unlock_token,         unique: true
-#   end
+  end
 # # end
