ab2d9c7deba3c08e46709f10728a3af2679621ca
naoyaaaa <23nakajima.naoya@gmail.com>
2023-07-17 15:44:03 +0900

    Update 20230714080212_create_goals.rb

--- db/migrate/20230714080212_create_goals.rb
+++ db/migrate/20230714080212_create_goals.rb
@@ -1,7 +1,7 @@
 class CreateGoals < ActiveRecord::Migration[7.0]
   def change
     create_table :goals do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, null: false
       t.string :title
       t.text :description
       t.string :category
