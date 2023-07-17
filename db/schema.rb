915f85f098ff22981b72daf0d69931bd688c7be4
naoyaaaa <23nakajima.naoya@gmail.com>
2023-07-17 10:45:15 +0900

    データベース修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_14_081826) do
   create_table "goals", charset: "utf8", force: :cascade do |t|
-    t.bigint "user_id"
     t.string "title"
     t.text "description"
     t.string "category"
@@ -20,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_081826) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -32,9 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_081826) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "goals", "users"
 end
