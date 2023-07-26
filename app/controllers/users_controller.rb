ac03ee32340570a7aea40b04ac82025150a7faf1
naoyaaaa <23nakajima.naoya@gmail.com>
2023-07-26 17:38:51 +0900

    宣言に条件をつけてみた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
     @goals = @user.goals.includes(:reflections).order(created_at: :desc)
     @reflection_goal_count = @goals.count { |goal| goal.reflections.count == 3 && goal.start_date <= Time.current.to_date - 2.days }
     @total_count = @reflection_goal_count 
+
+    @can_make_declaration = @reflection_goal_count 
   end
 end
