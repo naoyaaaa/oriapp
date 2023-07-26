39e8f2eee3cd0f40bbee63cd637e76554d0ece65
naoyaaaa <23nakajima.naoya@gmail.com>
2023-07-26 18:39:35 +0900

    JS実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,24 @@
 class LikesController < ApplicationController
+  before_action :set_goal
+
   def create
     like = current_user.likes.build(goal_id: params[:goal_id])
     like.save
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     like = Like.find_by(goal_id: params[:goal_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
+
+  def set_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
 end
